4fb19e460da74d6b886a5f247f4555781b227aef
gksriharsha <51872746+gksriharsha@users.noreply.github.com>
2024-06-13 17:03:12 -0700

    Update text_layer.py
    Updating pillow function usage as old functions are deprecated

--- synthtiger/layers/text_layer.py
+++ synthtiger/layers/text_layer.py
@@ -172,11 +172,11 @@ class TextLayer(Layer):
 
         if not vertical:
             ascent, descent = font.getmetrics()
-            width = font.getsize(text, direction=direction)[0]
+            width = font.getmask(text, direction=direction).size[0]
             height = ascent + descent
             bbox = [0, -ascent, width, height]
         else:
-            width, height = font.getsize(text, direction=direction)
+            width,height = font.getmask(text, direction=direction).size
             bbox = [-width // 2, 0, width, height]
 
         return bbox
